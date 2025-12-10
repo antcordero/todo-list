9aa63ce16b72947b0ab518f7326eb099d165b486
Antonio Cordero <antoniocorderomn@gmail.com>
2025-12-10 14:23:06 +0100

    Clase + apuntes - 10/12/2025

--- app.js
+++ app.js
@@ -3,39 +3,57 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var expressLayouts = require('express-ejs-layouts');
+var session = require('express-session');
+var expressSession = require('express-session');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+ 
 var app = express();
-
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+/** Ejemplo middleware */
+/**
+app.use( (req, res, next) => {
+  console.log('Nueva petición en ' + req.hostname + ' a las ' + (new Date()).toISOString());
+  next(); /** este hace que avance al siguiente middleware, si no lo lleva no hace nada*/
+/** 
+})
+*/
+
+/* insertar use para la session y que use la cookie */ 
+app.use(
+  expressSession({
+    secret: 'mi-clave',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+);
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+ 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+ 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+ 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
+ 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET admin layout. */
+router.get('/admin', function(req, res, next) {
+  res.render('index', { title: 'Express', layout: 'layout-admin' });
+});
+
+/**  Ruta del login - get login page */
+router.get('/login', function(req, res, next) {
+  res.render('login', {});
+});
+
+/**  Ruta del post */
+router.post('/login', function(req, res, next) {
+  if (req.body.name==="admin" && req.body.password==="admin") {
+    req.redirect("/admin");
+  } else {
+    res.render('login', {});
+  }
+  
+});
+
 module.exports = router;
